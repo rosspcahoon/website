bc7cb0e6445112bc3051ae1846d5877914eb9965
rosspcahoon <ross.p.cahoon@gmail.com>
2018-08-07 12:34:05 -0400

    Removed blogbar and framework, updated html slightly to introduce scroll bars on every page.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,18 +1,12 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
 import TabsReducer from './reducer_tabs';
 import ActiveTabReducer from './reducer_active_tab';
-import ActiveBlogTabReducer from './reducer_active_blog_tab';
-import BlogTabsReducer from './reducer_blog_tabs';
 import PostsReducer from './reducer_blog_posts';
 
 const rootReducer = combineReducers({
   posts: PostsReducer,
   tabs: TabsReducer,
   activeTab: ActiveTabReducer,
-  blogTabs: BlogTabsReducer,
-  activeBlogTab: ActiveBlogTabReducer,
-  form: formReducer
 });
 
 
--- src/reducers/reducer_blog_tabs.js
+++
@@ -1,6 +0,0 @@
-export default function() {
-    return [
-        { name: 'Add New Post', url: '/blog/new' },
-        { name: 'Blog Home', url: '/blog' }
-    ]
-}
